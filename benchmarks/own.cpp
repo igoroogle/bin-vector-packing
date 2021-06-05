c23c86f0811a34befb607b4b0483ea785eb4156a
Игорь Глушков <igoroogle@MacBook-Pro-Igor.local>
2021-06-05 21:22:14 +0300

    add benchmarks

---
+++ benchmarks/own.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <random>
+
+const std::vector<int> B = {100, 1000000};
+const std::vector<int> D = {5, 6, 7, 8, 9, 13, 22, 30, 44, 50, 55};
+const std::vector<int> NUM_OF_ITEMS = {5, 10, 15, 20, 30, 44, 53, 60, 62};
+int main()
+{
+    freopen("test.txt", "w", stdout);
+    std::mt19937 gen(-430238454);
+    std::uniform_int_distribution<> small(1, B[0] - 1);
+    std::uniform_int_distribution<> middle(1, B[1] - 1);
+    std::cout << B.size() * NUM_OF_ITEMS.size() << '\n'; //total tests
+    for (size_t cur_items = 0; cur_items < NUM_OF_ITEMS.size(); ++cur_items) {
+        int n = NUM_OF_ITEMS[cur_items];
+        for (size_t cur_d = 0; cur_d < D.size(); ++cur_d) {
+            int d = D[cur_d];
+
+
+            //gen small
+            std::cout << n << ' ' << d << '\n';
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < d - 1; ++j) {
+                    std::cout << small(gen) << ' ';
+                }
+                std::cout << small(gen) << '\n';
+            }
+
+            for (int i = 0; i < d - 1; ++i) {
+                std::cout << B[0] << ' ';
+            }
+            std::cout << B[0] << '\n';
+
+            //gen middle
+            std::cout << n << ' ' << d << '\n';
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < d - 1; ++j) {
+                    std::cout << middle(gen) << ' ';
+                }
+                std::cout << middle(gen) << '\n';
+            }
+
+            for (int i = 0; i < d - 1; ++i) {
+                std::cout << B[1] << ' ';
+            }
+            std::cout << B[1] << '\n';
+        }
+    }
+}
